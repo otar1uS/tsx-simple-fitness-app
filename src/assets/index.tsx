c5f88b69ee962be709f0e8819aa7d4e4d5806a27
Otari Pkhovelishvili <opkhovelishvili@gmail.com>
2023-03-26 12:51:55 +0400

    first commit

---
+++ src/assets/index.tsx
@@ -0,0 +1,33 @@
+import HomePageText from "./HomePageText.png";
+import HomePageGraphic from "./HomePageGraphic.png";
+import SponsorForbes from "./SponsorForbes.png";
+import SponsorRedBull from "./SponsorRedBull.png";
+import SponsorFortune from "./SponsorFortune.png";
+import Logo from "./Logo.png";
+import BenefitsPageGraphic from "./BenefitsPageGraphic.png";
+import Image1 from "./image1.png";
+import Image2 from "./image2.png";
+import Image3 from "./image3.png";
+import Image4 from "./image4.png";
+import Image5 from "./image5.png";
+import Image6 from "./image6.png";
+import ContactUsPageGraphic from "./contactUsPageGraphic.png";
+import Arrow from "./Arrow.png";
+
+export {
+  HomePageText,
+  HomePageGraphic,
+  SponsorForbes,
+  SponsorFortune,
+  SponsorRedBull,
+  Logo,
+  BenefitsPageGraphic,
+  Image1,
+  Image2,
+  Image3,
+  Image4,
+  Image5,
+  Image6,
+  ContactUsPageGraphic,
+  Arrow,
+};
